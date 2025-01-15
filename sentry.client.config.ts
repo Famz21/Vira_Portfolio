0e4ba7e857f85efcc5f0daad01686579023b82c4
Famz21 <famtomoe250@gmail.com>
2025-01-15 15:28:25 +0700

    Updated

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://3a96ce97282b706a361d28bd1aea7aa7@o4508643584180224.ingest.us.sentry.io/4508643588767744",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Add optional integrations for additional features
   integrations: [
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://3a96ce97282b706a361d28bd1aea7aa7@o4508643584180224.ingest.us.sentry.io/4508643588767744",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
